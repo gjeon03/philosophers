b7345a1de02916ea832e0bb54079f54c129b8fa3
gjeon03 <gjeon0321@gmail.com>
2022-03-28 17:14:31 +0900

    create struct philo

--- include/philo.h
+++ include/philo.h
@@ -9,13 +9,30 @@
 # include <string.h>
 # include <limits.h>
 
-typedef struct	s_philo {
-	int			philo_len; //철학자 수
-	int			time_life; //생존 시간
-	int			time_eat; //먹는 시간
-	int			time_sleep; //자는 시간
-	int			end_flag; //종료 카운트
-}				t_philo;
+typedef struct			s_philos
+{
+	int					is_eating;
+	int					fork_left;
+	int					fork_right;
+	int					eat_count;
+	int					pos;
+	t_table				*table;
+	unsigned long long	last_eat;
+}						t_philos;
+
+typedef struct			s_table {
+	int					num_philo; //철학자 수
+	int					time_life; //생존 시간
+	int					time_eat; //먹는 시간
+	int					time_sleep; //자는 시간
+	int					eat_count; //종료 카운트
+	int					is_dead;
+	t_philos			*philos;
+	pthread_mutex_t		*forks_m;
+	pthread_mutex_t		write_m;
+	pthread_mutex_t		end_m;
+	unsigned long long	start;
+}						t_table;
 
 int	ft_atoi(const char *str);
 
--- src/main.c
+++ src/main.c
@@ -1,49 +1,85 @@
 #include "philo.h"
 
-int	check_argv(t_philo *philo)
+int	check_argv(t_table *table)
 {
-	if (philo->philo_len <= 0)
+	if (table->num_philo <= 0)
 		return (1);
-	else if (philo->time_life <= 0)
+	else if (table->time_life <= 0)
 		return (1);
-	else if (philo->time_eat <= 0)
+	else if (table->time_eat <= 0)
 		return (1);
-	else if (philo->time_sleep <= 0)
+	else if (table->time_sleep <= 0)
 		return (1);
-	else if (philo->end_flag < 0)
+	else if (table->eat_count < 0)
 		return (1);
 	return (0);
 }
 
-void	philo_init(char *argv[], t_philo *philo)
+void	init_table(int argc, char *argv[], t_table *table)
 {
-	philo->philo_len = ft_atoi(argv[1]);
-	philo->time_life = ft_atoi(argv[2]);
-	philo->time_eat = ft_atoi(argv[3]);
-	philo->time_sleep = ft_atoi(argv[4]);
-	philo->end_flag = ft_atoi(argv[5]);
+	table->num_philo = ft_atoi(argv[1]);
+	table->time_life = ft_atoi(argv[2]);
+	table->time_eat = ft_atoi(argv[3]);
+	table->time_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		table->eat_count = ft_atoi(argv[5]);
+	else
+		table->eat_count = 0;
+}
+
+void	*thread_start(void *data)
+{
+	(void)data;
+	printf("hello\n");
+	return ("wwww\n");
+}
+
+int	create_table(t_table *table)
+{
+	pthread_t	*threads;
+	int			i;
+	char		*tmp;
+
+	threads = malloc(sizeof(pthread_t) * (table->num_philo + 1));
+	if (threads == NULL)
+		return (-1);
+	printf("create table\n");
+
+	i = 0;
+	tmp = 0;
+	printf("table num : %d\n", table->num_philo);
+	while (i < table->num_philo)
+	{
+		if (pthread_create(&(threads[i]), NULL,
+				thread_start, &tmp) != 0)
+			return (-1);
+		i++;
+		// printf("tmp: %s\n", tmp);
+	}
+	return (0);
 }
 
 int main(int argc, char *argv[])
 {
-	t_philo	philo;
+	t_table	table;
 
-	if (argc < 6 || argc > 6)
+	if (argc < 5 || 6 < argc)
 	{
 		printf("please input\n");
 		return (1);
 	}
-	philo_init(argv, &philo);
-
-	if (check_argv(&philo))
+	init_table(argc, argv, &table);
+	if (check_argv(&table))
 	{
 		printf("check input\n");
 		return (1);
 	}
-	printf("philo_len = %d\n", philo.philo_len);
-	printf("time_life = %d\n", philo.time_life);
-	printf("time_eat = %d\n", philo.time_eat);
-	printf("time_sleep = %d\n", philo.time_sleep);
-	printf("end_flag = %d\n", philo.end_flag);
+	create_table(&table);
+
+	// printf("num_philo = %d\n", table.num_philo);
+	// printf("time_life = %d\n", table.time_life);
+	// printf("time_eat = %d\n", table.time_eat);
+	// printf("time_sleep = %d\n", table.time_sleep);
+	// printf("eat_count = %d\n", table.eat_count);
 	return (0);
 }
